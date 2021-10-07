3910e4ea9c3c26cf7095aa6989675204db85d07d
Sohil L Shrestha <sohil.shrestha@mavs.uta.edu>
2021-10-07 14:25:22 -0500

    correct URL to GPT

--- GPT/download_model.py
+++ GPT/download_model.py
@@ -9,7 +9,7 @@ if len(sys.argv) != 2:
 
 model = sys.argv[1]
 
-subdir = os.path.join('src/models', model)
+subdir = os.path.join('models', model)
 if not os.path.exists(subdir):
     os.makedirs(subdir)
 subdir = subdir.replace('\\','/') # needed for Windows
